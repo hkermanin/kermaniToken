64586ccb4de7dd8ab581ae6c3c0debb4c0a53d81
hkermanin <97584991+hkermanin@users.noreply.github.com>
2023-07-20 11:50:56 +0330

    Update index.js
    Update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x2ff384c7db1564e85c97ff43ff9017714adb3645";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
